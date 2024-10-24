7f2c29379ba2e826bbb902b45821bb76164b70f4
Ben Smylie <bensmylie@gmail.com>
2024-10-25 00:02:27 +0100

    Update grid with solve() method

--- PydokuUI.py
+++ PydokuUI.py
@@ -20,7 +20,8 @@ class PydokuApp(QWidget):
     for row in range(9):
       for col in range(9):
         cell_value = str(self.game.get_board()[row][col])
-        cell = QLineEdit(text=cell_value)
+        cell = QLineEdit()
+        cell.setText(cell_value)
         # set size
         cell.setFixedHeight(32)
         cell.setFixedWidth(32)
@@ -29,30 +30,40 @@ class PydokuApp(QWidget):
         font_weight = "400"
         font_size = "20pt"
         if int(cell_value) in range(1, 10):
+          cell.setDisabled(True)
           font_weight = "700"
-          cell.setEnabled(False)
         # set rules
         cell.setMaxLength(1)
         cell.setInputMask("d")
-        
+        # format grid appearance
         if (row // 3 in [0, 2] and col // 3 in [0, 2]) or (row // 3 == 1 and col // 3 == 1):
           cell_background = "white"
         else:
           cell_background = "grey"
-
         cell.setStyleSheet(f"background: {cell_background}; color: black; font-weight: {font_weight}; font-size: {font_size}")
         # add to layout
         grid_layout.addWidget(cell, row, col)
-
+        
     solve_button = QPushButton(text="Solve")
+    solve_button.clicked.connect(self.solve)
     
     # set main layout
     self.grid_layout = grid_layout
     grid_container.addLayout(grid_layout)
     main_layout.addLayout(grid_container)
-    # main_layout.addWidget(solve_button)
+    main_layout.addWidget(solve_button)
     self.setLayout(main_layout)
 
+  def update(self):
+    for row in range(9):
+      for col in range(9):
+        # set widget at {row}:{col} to board value
+        cell_value = str(self.game.get_board()[row][col])
+        self.grid_layout.itemAtPosition(row, col).widget().setText(cell_value)
+
+  def solve(self):
+    self.game.solve()
+    self.update()
 
 
 if __name__ == "__main__":
